6d4f0c3448fc734b738d0ad22006310a1e938b68
Daljeet Singh Chhabra <idaljeetsingh@outlook.com>
2020-01-02 10:21:08 +0530

    Updated GUI to use PyQt5. Added ablity to execute script as sudo.

--- main.py
+++ main.py
@@ -3,113 +3,105 @@
     Language    :   Python
     File Name   :   main.py
     Date Created    :   28-12-2019
-    Last Modified   :   31-12-2019
+    Last Modified   :   01-01-2020
 """
-
-from tkinter import Entry, Label, StringVar, Tk, Listbox, Scrollbar, Button, END
-from paramiko import SSHClient, AutoAddPolicy, sftp
-from tkinter.filedialog import askopenfile
+from PyQt5 import uic, QtWidgets, QtGui
+from paramiko import SSHClient, AutoAddPolicy
 from os.path import basename
+import sys
 
-local_script_path = None
 
+class Executer(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Executer, self).__init__()
+        uic.loadUi('Executer.ui', self)
+        # self.setWindowIcon(QtGui.QIcon("icon.png"))
+        self.execute_button.clicked.connect(self.execute_button_clicked)
+        self.script_entry.returnPressed.connect(self.get_script_file)
+        self.clear_button.clicked.connect(self.clear_button_clicked)
 
-def view_command():
-    server_response.delete(0, END)
-    # Connect to remote host
-    client = SSHClient()
-    client.set_missing_host_key_policy(AutoAddPolicy())
-    client.connect(host_text.get(), username=uname_text.get(), password=password_text.get())
+    def get_script_file(self):
+        file = QtWidgets.QFileDialog.getOpenFileName(self, 'Open file')
+        if file[0]:
+            # print(file[0])
+            # print(basename(file[0]))
+            self.script_entry.setText(file[0])
+        else:
+            print('No File Selected')
 
-    # SSHClient.exec_command() returns the tuple (stdin,stdout,stderr)
-    try:
-        global local_script_path
+    def execute_button_clicked(self):
+        host = self.host_entry.text()
+        port = self.port_entry.text()
+        script_path = self.script_entry.text()
+        usr = self.username_entry.text()
+        pwd = self.password_entry.text()
+        as_sudo = self.as_sudo.isChecked()
+        self.execute_script(host, port, usr, pwd, script_path, as_sudo)
 
-        # Setup sftp connection and transmit this script
-        sftp = client.open_sftp()
-        sftp.put(local_script_path.name, f'/tmp/{basename(local_script_path.name)}')
-        sftp.close()
-        path_to_script_on_server = '/tmp/' + basename(local_script_path.name)
+    def clear_button_clicked(self):
+        self.host_entry.setText('')
+        self.port_entry.setText('')
+        self.script_entry.setText('')
+        self.username_entry.setText('')
+        self.password_entry.setText('')
+        self.server_response.setReadOnly(False)
+        self.server_response.setText('')
+        self.server_response.setReadOnly(True)
+        self.as_sudo.setChecked(False)
+
+    def execute_script(self, host, port, usr, pwd, script_path, as_sudo):
         try:
-            stdout = client.exec_command('chmod +x ' + path_to_script_on_server)
-            stdout = client.exec_command(path_to_script_on_server)[1]
-            for line in stdout:
-                # Process each line in the remote output
-                # print(line)
-                server_response.insert(END, line)
+            # Connect to remote host                                            192.168.195.130
+            client = SSHClient()
+            client.set_missing_host_key_policy(AutoAddPolicy())
+            client.connect(host, port=int(port), username=usr, password=pwd)
+            try:
+                # Setup sftp connection and transmit this script
+                sftp = client.open_sftp()
+                sftp.put(script_path, f'/tmp/{basename(script_path)}')
+                sftp.close()
+                path_to_script_on_server = '/tmp/' + basename(script_path)
+                if as_sudo:  # Script will be executed as sudo
+                    try:
+                        stdin, stdout, stderr = client.exec_command('sudo chmod +x ' + path_to_script_on_server)
+                        stdin.write(pwd + '\n')
+                        stdin.flush()
+
+                        stdin, stdout, stderr = client.exec_command('sudo ' + path_to_script_on_server, get_pty=True)
+                        stdin.write(pwd + '\n')
+                        stdin.flush()
+
+                        output = stdout.read().splitlines()
+                        self.server_response.setReadOnly(False)
+                        for line in range(1, len(output)):
+                            # Process each line in the remote output
+                            # print(str(output[line], 'utf-8'))
+                            self.server_response.append(str(output[line], 'utf-8'))
+                        stdout.flush()
+                        self.server_response.setReadOnly(True)
+                        client.close()
+                    except:
+                        print('Error executing the script as SUDO')
+                else:  # Script will be executed without root
+                    try:
+                        stdout = client.exec_command('chmod +x ' + path_to_script_on_server)
+                        stdout = client.exec_command(path_to_script_on_server)[1]
+                        self.server_response.setReadOnly(False)
+                        for line in stdout:
+                            # Process each line in the remote output
+                            # print(line)
+                            self.server_response.append(str(line))
+                        client.close()
+                        self.server_response.setReadOnly(True)
+                    except:
+                        print('Error executing the script.')
+            except:
+                print('Error making SFTP connection')
         except:
-            print('Error executing the script')
-    except:
-        print('Execution failed')
-    client.close()
-    server_response.insert(END)
+            print('Connection to SSH server failed')
 
 
-def file_picker():
-    global local_script_path
-    local_script_path = askopenfile()
-    script_path_entry.delete(0, END)
-    script_path_entry.insert(END, local_script_path.name)
-    # print(local_script_path.name)
-
-
-def clear():
-    host_entry.delete(0, END)
-    script_path_entry.delete(0, END)
-    uname_entry.delete(0, END)
-    password_entry.delete(0, END)
-    server_response.delete(0, END)
-
-window = Tk()
-window.title("Server Script Executer")
-
-host = Label(window, text="Host")
-host.grid(row=0, column=0)
-
-host_text = StringVar()
-host_entry = Entry(window, textvariable=host_text)
-host_entry.grid(row=0, column=1)
-
-script_path_label = Label(window, text="Script Path")
-script_path_label.grid(row=0, column=3)
-
-script_path_text = StringVar()
-script_path_entry = Entry(window, textvariable=script_path_text)
-script_path_entry.grid(row=0, column=4)
-
-file_picker = Button(window, text="Select", width=5, command=file_picker)
-file_picker.grid(row=0, column=6)
-
-uname_label = Label(window, text="Username")
-uname_label.grid(row=1, column=0)
-
-uname_text = StringVar()
-uname_entry = Entry(window, textvariable=uname_text)
-uname_entry.grid(row=1, column=1)
-
-password_label = Label(window, text="Password")
-password_label.grid(row=1, column=3)
-
-password_text = StringVar()
-password_entry = Entry(window, textvariable=password_text)
-password_entry.config(show='*')
-password_entry.grid(row=1, column=4)
-
-server_response = Listbox(window, height=10, width=40)
-server_response.grid(row=3, column=0, rowspan=6, columnspan=3)
-
-s_bar = Scrollbar(window)
-s_bar.grid(row=2, column=3, rowspan=6)
-
-server_response.configure(yscrollcommand=s_bar.set)
-s_bar.configure(command=server_response.yview)
-
-# server_response.bind('<<ListboxSelect>>', get_selected_row)
-
-execute_button = Button(window, text="Execute", width=12, command=view_command)
-execute_button.grid(row=4, column=4)
-
-clear_button = Button(window, text="Clear", width=5, command=clear)
-clear_button.grid(row=5, column=4)
-
-window.mainloop()
+app = QtWidgets.QApplication(sys.argv)
+window = Executer()
+window.show()
+sys.exit(app.exec_())
